fa45517f0f03837cf8adf9283fa6472086f76088
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2020-11-09 20:38:12 -0300

    Add listing 10.7

--- chapter10/listing10-6.ts
+++ chapter10/listing10-6.ts
@@ -1,1 +1,14 @@
-export default class PrimePrinter {}
+import RowColumnPagePrinter from './listing10-7';
+
+export default class PrimePrinter {
+    static printPrimes() {
+        const ROWS_PER_PAGE = 40;
+        const COLUMNS_PER_PAGE = 4;
+        const data = [1, 2, 3, 45, 4, 67, 8, 798, 4, 54, 56, 421, 5, 45, 6, 7, 8, 456, 4, 654, 230];
+        const tablePrinter = new RowColumnPagePrinter(ROWS_PER_PAGE, COLUMNS_PER_PAGE, 'HEADER');
+
+        tablePrinter.print(data);
+    }
+}
+
+PrimePrinter.printPrimes();
--- chapter10/listing10-7.ts
+++ chapter10/listing10-7.ts
@@ -4,13 +4,43 @@ export default class RowColumnPagePrinter {
     private numbersPerPage: number;
     private pageHeader: string;
 
-    constructor(
-        rowsPerPage: number,
-        columnsPerPage: number,
-        pageHeader: string
-    ) {
+    constructor(rowsPerPage: number, columnsPerPage: number, pageHeader: string) {
         this.rowsPerPage = rowsPerPage;
         this.columnsPerPage = columnsPerPage;
         this.pageHeader = pageHeader;
+        this.numbersPerPage = rowsPerPage * columnsPerPage;
+    }
+
+    print(data: number[]) {
+        let pageNumber = 1;
+        for (let firstIndexOnPage = 0; firstIndexOnPage < data.length; firstIndexOnPage += this.numbersPerPage) {
+            let lastIndexOnPage = Math.min(firstIndexOnPage + this.numbersPerPage - 1, data.length - 1);
+            this.printPageHeader(this.pageHeader, pageNumber);
+            this.printPage(firstIndexOnPage, lastIndexOnPage, data);
+            process.stdout.write('\f');
+            pageNumber++;
+        }
+    }
+
+    private printPage(firstIndexOnPage: number, lastIndexOnPage: number, data: number[]) {
+        let firstIndexOfLastRowOnPage = firstIndexOnPage + this.rowsPerPage - 1;
+        for (let firstIndexInRow = firstIndexOnPage; firstIndexInRow <= firstIndexOfLastRowOnPage; firstIndexInRow++) {
+            this.printRow(firstIndexInRow, lastIndexOnPage, data);
+            console.log('');
+        }
+    }
+
+    private printRow(firstIndexInRow: number, lastIndexOnPage: number, data: number[]) {
+        for (let column = 0; column < this.columnsPerPage; column++) {
+            let index = firstIndexInRow + column * this.rowsPerPage;
+            if (index <= lastIndexOnPage) {
+                process.stdout.write(data[index].toString().padEnd(10, ' '));
+            }
+        }
+    }
+
+    private printPageHeader(pageHeader: string, pageNumber: number) {
+        console.log(pageHeader + ' --- Page ' + pageNumber);
+        console.log('');
     }
 }
